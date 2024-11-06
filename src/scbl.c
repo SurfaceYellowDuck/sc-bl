e917f7a4aa480cbfef739ad45c7daad690c63290
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2024-11-06 22:01:46 +0300

    chages: changed scbl.c changed define MAX_XMODEM_RX_LEN from PLF_MEM_SIZE to PLF_TCM_SIZE because we have not SRAM memory removed leds_print_info and dips_print_info calls from scbl.c to reduce .hex output file size removed hex_leds_task and leds_task from scbl.c to reduce .hex output file size

--- src/scbl.c
+++ src/scbl.c
@@ -18,7 +18,7 @@
 #define FW_VER_CFG "scr1_RC"
 #define COPYRIGHT_STR "Copyright (C) 2015-2021 Syntacore. All rights reserved."
 
-#define MAX_XMODEM_RX_LEN PLF_MEM_SIZE
+#define MAX_XMODEM_RX_LEN PLF_TCM_SIZE
 
 #define LEDS_TASK_DELAY_MS  200
 #define MAX_HEX_LEDS        8
@@ -126,8 +126,8 @@ static void cmd_plf_info(void *arg)
     hwinfo();
     uart_puts("Platform configuration:\n");
     uart_print_info();
-    leds_print_info();
-    dips_print_info();
+    // leds_print_info();
+    // dips_print_info();
     btn_print_info();
 }
 
@@ -213,98 +213,17 @@ void usage(void)
     }
 }
 
-static void hex_leds_task(void)
-{
-#ifdef PLF_HEXLED_ADDR
-    static uint32_t id_val, idx_i, idx_j;
-    static sys_tick_t start_time;
-    sys_tick_t t2;
-
-    if (id_val == 0) {
-        for (int i = 0; i < MAX_HEX_LEDS; i++) {
-            sc1f_leds_hex(i, 0x0);
-        }
-        id_val = get_build_id();
-        start_time = now();
-        idx_i = 1;
-        return;
-    }
-
-    t2 = now();
-    if (t2 - start_time < ms2ticks(LEDS_TASK_DELAY_MS / 6)) {
-        return;
-    }
-    start_time = now();
-
-    if (idx_i <= MAX_HEX_LEDS) {
-        if (idx_j < 9 - idx_i) {
-            if (idx_j) {
-                for (int i = 1; i < MAX_HEX_LEDS - idx_i; i++) {
-                    sc1f_leds_hex(MAX_HEX_LEDS - 1 - i, 0x0);
-                }
-                sc1f_leds_hex_digit(MAX_HEX_LEDS - 1 - idx_j,
-                    id_val >> 4 * (MAX_HEX_LEDS - idx_i));
-            } else {
-                sc1f_leds_hex_digit(MAX_HEX_LEDS - 1 - idx_j, 0xF);
-            }
-            idx_j++;
-        } else {
-            idx_j = 0;
-            idx_i++;
-        }
-    }
-#endif // PLF_HEXLED_ADDR
-}
-
-static void leds_task(void)
-{
-#ifdef PLF_PINLED_NUM
-#if PLF_PINLED_NUM > 0
-    static unsigned long leds_val;
-    static sys_tick_t start_time;
-    static int right;
-    sys_tick_t t2;
-
-    if (start_time == 0) {
-        start_time = now();
-        leds_val = 0x1;
-    }
-
-    t2 = now();
-    if (t2 - start_time < ms2ticks(LEDS_TASK_DELAY_MS)) {
-        return;
-    }
-
-    start_time = now();
-    sc1f_leds_set(leds_val);
-
-    if (right) {
-        leds_val <<= 1;
-    } else {
-        leds_val >>= 1;
-    }
-    if (leds_val >= (1 << (PLF_PINLED_NUM - 1))) {
-        right = 0;
-    }
-    if (leds_val <= 0x1) {
-        right = 1;
-        leds_val = 0x1;
-    }
-#endif // PLF_PINLED_NUM > 0
-#endif // PLF_PINLED_NUM
-}
-
 void indication_tasks(void)
 {
-    hex_leds_task();
-    leds_task();
+    // hex_leds_task();
+    // leds_task();
 }
 
 int main(void)
 {
     scr_rtc_init();
     sc1f_uart_init();
-    sc1f_leds_init();
+    // sc1f_leds_init();
 
     show_header();
     hwinfo();
