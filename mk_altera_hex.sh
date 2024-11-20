f211ff74890dcb7f2a9bb7791155cd567c012932
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2024-11-20 17:05:23 +0300

    fix: fixed syntax error in mk_altera_hex.sh Error text: ./mk_altera_hex.sh: line 28: 31 + : syntax error: operand expected (error token is "+ ") An error occurred due to the use of outdated syntax in the expression $[$cs+$val]. Fixed it by using $((...)) expression syntax. Also fixed shellcheck notes.

--- mk_altera_hex.sh
+++ mk_altera_hex.sh
@@ -8,30 +8,33 @@ readonly BYTES_PER_LINE=8
 readonly BYTES_PER_READ=64
 readonly LINES_IN_BUFFER=$BYTES_PER_READ/$BYTES_PER_LINE
 
-readonly FORMAT_STR_PREFIX=$(printf '%%s:%02X%%04X00' $BYTES_PER_LINE)
+FORMAT_STR_PREFIX=$(printf '%%s:%02X%%04X00' $BYTES_PER_LINE)
+readonly FORMAT_STR_PREFIX
 
-printf "" >$2
+printf "" >"$2"
 outaddr=0
-wcstr=$(wc -c $1)
+wcstr=$(wc -c "$1")
 fsize=${wcstr% *}
-for (( addr = 0 ; addr < $fsize ; addr += $BYTES_PER_READ )) ; do
-    bytes=( $(od -An -t u1 -N $BYTES_PER_READ --width=1000 -j $addr $1) )
+for (( addr = 0 ; addr < fsize ; addr += BYTES_PER_READ )) ; do
+    bytes=()
+    while IFS=$'' read -r line; do array+=("$line"); done < <((od -An -t u1 -N $BYTES_PER_READ --width=1000 -j $addr "$1"))
     out_str=""
     ii=0
-    for (( n = 0 ; n < $LINES_IN_BUFFER ; n++ )) ; do
-        printf -v out_str $FORMAT_STR_PREFIX "$out_str" $outaddr
-        cs=$[$outaddr/256+$outaddr%256]
+    for (( n = 0 ; n < LINES_IN_BUFFER ; n++ )) ; do
+        # shellcheck disable=SC2059
+        printf -v out_str "$FORMAT_STR_PREFIX" "$out_str" $outaddr
+        cs=$((outaddr/256+outaddr%256))
         line_bytes=""
-        for (( i = $ii+$BYTES_PER_LINE-1 ; i >= $ii ; i--)) ; do
+        for (( i = ii+BYTES_PER_LINE-1 ; i >= ii ; i--)) ; do
             val=${bytes[$i]}
-            printf -v line_bytes '%s%02X' "$line_bytes" $val
-            cs=$[$cs+$val]
+            printf -v line_bytes '%s%02X' "$line_bytes" "$val"
+            cs=$((cs+val))
         done
-        cs=$[$cs+$BYTES_PER_LINE]
-        printf -v out_str '%s%s%02X\n' "$out_str" "$line_bytes" $[(0x100-$cs%256)%256]
-        outaddr=$[$outaddr+1]
-        ii=$[$ii+$BYTES_PER_LINE]
+        cs=$((cs+BYTES_PER_LINE))
+        printf -v out_str '%s%s%02X\n' "$out_str" "$line_bytes" $(((0x100-cs%256)%256))
+        outaddr=$((outaddr+1))
+        ii=$((ii+BYTES_PER_LINE))
     done
-    printf '%s' "$out_str" >>$2
+    printf '%s' "$out_str" >>"$2"
 done
-printf ":00000001FF\n" >>$2
+printf ":00000001FF\n" >>"$2"
