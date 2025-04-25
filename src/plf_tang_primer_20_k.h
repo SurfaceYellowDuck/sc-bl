35c122f9de8435c9b4a3e1eb9c32b3cb6972d538
SurfaceYellowDuck <lukonenko.nikitka@mail.ru>
2025-04-25 18:52:38 +0300

    changed addrs to work with ddr

--- src/plf_tang_primer_20_k.h
+++ src/plf_tang_primer_20_k.h
@@ -21,25 +21,29 @@
 #define PLF_TCM_SIZE     (4*1024)
 #define PLF_TCM_ATTR     0
 #define PLF_TCM_NAME     "TCM"
+
 #define PLF_MTIMER_BASE  (0xF0040000)
 #define PLF_MTIMER_SIZE  (0x1000)
 #define PLF_MTIMER_ATTR  0
 #define PLF_MTIMER_NAME  "MTimer"
+
 #define PLF_MMIO_BASE    (0xFF000000)
 #define PLF_MMIO_SIZE    (0x100000)
 #define PLF_MMIO_ATTR    0
 #define PLF_MMIO_NAME    "MMIO"
-#define PLF_OCROM_BASE   (0xFFEE0000)
+
+#define PLF_OCROM_BASE   (0xFFFF0000)
 #define PLF_OCROM_SIZE   (32*1024)
 #define PLF_OCROM_ATTR   0
 #define PLF_OCROM_NAME   "On-Chip ROM"
+
 #define PLF_MEM_MAP                                                     \
     {PLF_TCM_BASE, PLF_TCM_SIZE, PLF_TCM_ATTR, PLF_TCM_NAME},           \
     {PLF_MTIMER_BASE, PLF_MTIMER_SIZE, PLF_MTIMER_ATTR, PLF_MTIMER_NAME}, \
     {PLF_MMIO_BASE, PLF_MMIO_SIZE, PLF_MMIO_ATTR, PLF_MMIO_NAME},       \
     {PLF_OCROM_BASE, PLF_OCROM_SIZE, PLF_OCROM_ATTR, PLF_OCROM_NAME}
 // FPGA UART port
-#define PLF_UART0_BASE   (PLF_MMIO_BASE + 0xDF0000)
+#define PLF_UART0_BASE   (PLF_MMIO_BASE + 0x10000)
 #define PLF_UART0_16550
 #define PLF_UART0_IRQ 0
 
